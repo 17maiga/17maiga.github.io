d8af77356dac8e0ddfcafeb653f2aed6f58ffeff
17maiga <henri.saudubray@gmail.com>
2022-05-19 15:15:48 +0200

    formatting & trying out vscode.dev

--- projects/banane-clicker/assets/scripts/game.js
+++ projects/banane-clicker/assets/scripts/game.js
@@ -1,4 +1,4 @@
 function sayHi() {
-    let output = document.getElementById("output");
-    output.innerText = (output.innerText === "hello") ? "hi" : "hello";
+  let output = document.getElementById("output");
+  output.innerText = output.innerText === "hello" ? "hi" : "hello";
 }
